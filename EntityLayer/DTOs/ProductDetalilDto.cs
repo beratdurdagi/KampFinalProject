e57075fb00f9198c72d21cbfcaf5b6d6b6a0c971
karalakrepp <karalakrep@hotmail.com>
2024-03-21 05:08:39 +0300

    Added DTOs

---
+++ EntityLayer/DTOs/ProductDetalilDto.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.DTOs
+{
+    public class ProductDetalilDto :IDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public short UnitsInStock { get; set; }
+    }
+}
