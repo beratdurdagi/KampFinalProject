5fea66dd8ad053a0b8a9ab8301674c8f444739ca
karalakrepp <karalakrep@hotmail.com>
2024-03-21 04:31:33 +0300

    Created ICategoryService and CategoryManager

---
+++ BusinessLayer/Concretes/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concretes
+{
+    public class CategoryManager : ICategoryService
+
+
+    {
+
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            //Business rules
+            
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(P => P.CategoryId == categoryId);
+        }
+    }
+}
