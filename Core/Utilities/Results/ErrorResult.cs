6351821abdb02596be7d2707a86a146c4878f592
karalakrepp <karalakrep@hotmail.com>
2024-03-21 18:27:02 +0300

    Added Utilities

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results;
+
+public class FalseResult : Result
+{
+
+    public FalseResult(string message) :base(false,message)
+    {
+        
+    }
+
+    public FalseResult() :base(false) { }
+    
+
+}
