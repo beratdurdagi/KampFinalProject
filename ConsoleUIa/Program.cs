44039c311ebce64edc826a80f664c4678779fe63
karalakrepp <karalakrep@hotmail.com>
2024-04-12 06:58:12 +0300

    added someting for test

--- ConsoleUIa/Program.cs
+++ ConsoleUIa/Program.cs
@@ -9,15 +9,44 @@ namespace ConsoleUIa
 
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EFProductDal());
+            
+            ProductTest();
+            //CategoryTest();
 
-            CategoryManager categoryManager =new CategoryManager(new EFCategoryDal());
+        }
+        private static void CategoryTest()
+        {
+
+
+            CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
 
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
-                
+
             }
         }
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true )
+            {
+                foreach (var product in result.Data) 
+                {
+                    Console.WriteLine(product.CategoryName);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+          
+        }
+
+       
     }
 }
