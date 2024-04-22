989cc19dd07a54ae3296e0f09ee232bf7745ff18
karalakrepp <karalakrep@hotmail.com>
2024-04-22 05:08:14 +0300

    appsettingsjson changed

--- KampFinalProject/Program.cs
+++ KampFinalProject/Program.cs
@@ -1,25 +1,53 @@
 ﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using BusinessLayer.Abstract;
-using BusinessLayer.Concretes;
+
 using BusinessLayer.DependencyResolvers.AutoFac;
-using DataAccessLayer.Abstract;
-using DataAccessLayer.Concrete.EntityFramework;
+using Core.DependecyResolvers;
+
+using Core.Extensions;
+
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
 {
     builder.RegisterModule(new AutofacBusinessModule());
-}
-);
+});
+
+//auth
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                    };
+                });
+
+builder.Services.AddDependencyResolvers([new CoreModule()]);
+// Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 
 var app = builder.Build();
@@ -30,6 +58,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(builder => builder.WithOrigins("*").AllowAnyHeader());
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthentication();
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
